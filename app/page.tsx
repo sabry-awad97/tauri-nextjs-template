8a9fbf36289ae1207acbf9e8d52030b9de9977f5
Sabry Awad <dr.sabry97@gmail.com>
2024-01-10 18:29:43 +0200

    🎉 feat: Add Greet component and API request from client 📝 chore: Update package dependencies 🐛 fix: Fix greeting message formatting in main.rs 🔧 chore: Update tailwind.config.ts

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Greet from "./components/Greet";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello world!</h1>
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Greet />
     </div>
   );
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,34 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+#[derive(Default)]
+struct VisitorCounter {
+    visitor: AtomicUsize,
+}
+
+impl VisitorCounter {
+    fn increment(&self) {
+        self.visitor.fetch_add(1, Ordering::Relaxed);
+    }
+
+    fn get(&self) -> usize {
+        self.visitor.load(Ordering::Relaxed)
+    }
+}
+
+#[tauri::command]
+async fn greet(name: String, counter: tauri::State<'_, VisitorCounter>) -> Result<String, String> {
+    counter.increment();
+    println!("Visitor: {}", counter.get());
+    Ok(format!("Hello {name}!"))
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![])
+        .manage(VisitorCounter::default())
+        .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
